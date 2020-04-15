1d89e6c1c0d7b7c9b333480cff45ddcbb7c129e6
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-04-15 15:03:47 +1000

    impl: add getter and setter in acitivity session and change status field from string to enum

---
+++ app/src/main/java/edu/monash/student/happyactive/ActivityPackages/ActivityPackageStatus.java
@@ -0,0 +1,4 @@
+package edu.monash.student.happyactive.ActivityPackages;
+
+public enum ActivityPackageStatus {
+}
--- app/src/main/java/edu/monash/student/happyactive/data/entities/ActivitySession.java
+++ app/src/main/java/edu/monash/student/happyactive/data/entities/ActivitySession.java
@@ -5,6 +5,8 @@ import androidx.room.PrimaryKey;
 
 import java.util.Date;
 
+import edu.monash.student.happyactive.ActivityPackages.ActivityPackageStatus;
+
 @Entity
 public class ActivitySession {
     @PrimaryKey(autoGenerate = true)
@@ -12,8 +14,63 @@ public class ActivitySession {
 
     public long activityId;
     public long currentTaskId;
-    public String status;
+    public ActivityPackageStatus status;
     public int stepCount;
-    public Date StartDateTime;
-    public Date CompletedDateTime;
+    public Date startDateTime;
+    public Date completedDateTime;
+
+    public ActivitySession(long activityId, long currentTaskId, ActivityPackageStatus status) {
+        this.activityId = activityId;
+        this.currentTaskId = currentTaskId;
+        this.status = status;
+        this.startDateTime = new Date();
+    }
+
+    public long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(long activityId) {
+        this.activityId = activityId;
+    }
+
+    public long getCurrentTaskId() {
+        return currentTaskId;
+    }
+
+    public void setCurrentTaskId(long currentTaskId) {
+        this.currentTaskId = currentTaskId;
+    }
+
+    public ActivityPackageStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ActivityPackageStatus status) {
+        this.status = status;
+    }
+
+    public int getStepCount() {
+        return stepCount;
+    }
+
+    public void setStepCount(int stepCount) {
+        this.stepCount = stepCount;
+    }
+
+    public Date getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(Date startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public Date getCompletedDateTime() {
+        return completedDateTime;
+    }
+
+    public void setCompletedDateTime(Date completedDateTime) {
+        this.completedDateTime = completedDateTime;
+    }
 }
