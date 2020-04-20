3e31f644632af914397a15b5808aa00531e887b2
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-04-21 01:03:51 +1000

    impl: let user to take a photo afte activity is finised

--- app/src/main/java/edu/monash/student/happyactive/fragments/CameraFragment.java
+++ app/src/main/java/edu/monash/student/happyactive/fragments/CameraFragment.java
@@ -1,24 +1,43 @@
 package edu.monash.student.happyactive.fragments;
 
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import edu.monash.student.happyactive.R;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class CameraFragment extends Fragment {
     String currentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageView mPhotoImageView;
+    private Uri photoURI;
+
 
     public CameraFragment() { }
 
@@ -37,12 +56,21 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View view =  inflater.inflate(R.layout.fragment_camera, container, false);
+        mPhotoImageView = view.findViewById(R.id.session_photo);
+        mPhotoImageView.setRotation(90);
+        view.findViewById(R.id.camera_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+        return view;
     }
 
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageFileName = "SessionID" + timeStamp + "";
         File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
@@ -52,5 +80,48 @@ public class CameraFragment extends Fragment {
 
         currentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.e("File", "dispatchTakePictureIntent: ", ex.fillInStackTrace());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(getContext(),
+                        "edu.monash.student.happyactive.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+
+
+                List<ResolveInfo> resInfoList = getContext().getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
+                for (ResolveInfo resolveInfo : resInfoList) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    getContext().grantUriPermission(packageName, photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
+
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), photoURI);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mPhotoImageView.setImageBitmap(bitmap);
+        }
     }
 }
