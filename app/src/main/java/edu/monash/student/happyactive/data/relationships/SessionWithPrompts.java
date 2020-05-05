84aa1c105b40e715d0cb6f7560fc91cc57a3567d
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-05-05 12:59:06 +1000

    create intereactive task prompt entity and bind it with session

--- app/src/main/java/edu/monash/student/happyactive/data/relationships/SessionWithPrompts.java
+++ app/src/main/java/edu/monash/student/happyactive/data/relationships/SessionWithPrompts.java
@@ -1,4 +1,18 @@
 package edu.monash.student.happyactive.data.relationships;
 
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+import edu.monash.student.happyactive.data.entities.ActivityJournal;
+import edu.monash.student.happyactive.data.entities.ActivitySession;
+
 public class SessionWithPrompts {
+    @Embedded
+    public ActivitySession activitySession;
+
+    @Relation(
+            parentColumn = "id",
+            entityColumn = "sessionId"
+    )
+    public ActivityJournal activityJournal;
 }
