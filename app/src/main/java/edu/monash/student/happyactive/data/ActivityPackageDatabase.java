5d34ecba2a061dd7d6687c8e6f4e66ddf9fe67d5
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-05-07 09:46:40 +1000

    load data into db

--- app/src/main/java/edu/monash/student/happyactive/data/ActivityPackageDatabase.java
+++ app/src/main/java/edu/monash/student/happyactive/data/ActivityPackageDatabase.java
@@ -42,16 +42,6 @@ public abstract class ActivityPackageDatabase extends RoomDatabase {
     
     private static ActivityPackageDatabase INSTANCE;
 
-    static final Migration MIGRATION_1_2 = new Migration(1, 2) {
-        @Override
-        public void migrate(SupportSQLiteDatabase database) {
-            database.execSQL("ALTER TABLE ActivityPackage ADD COLUMN activityLevel INTEGER NOT NULL DEFAULT 0");
-            database.execSQL("CREATE TABLE IF NOT EXISTS `UserPref` (`id` INTEGER NOT NULL, `hobbyList` TEXT, `exerciseFreq` INTEGER, `grandparentInteractionFreq` INTEGER, `gardenAccess` INTEGER, `dogAccess` INTEGER, PRIMARY KEY(`id`))");
-            database.execSQL("CREATE TABLE IF NOT EXISTS `UserScore` (`id` INTEGER NOT NULL, `currentScore` INTEGER NOT NULL, PRIMARY KEY(`id`))");
-            database.execSQL("INSERT INTO UserPref (id) VALUES (1)");
-            database.execSQL("INSERT INTO UserScore (id, currentScore) VALUES (1, 0)");
-        }
-    };
 
 
     public static ActivityPackageDatabase getDatabase(final Context context) {
@@ -61,7 +51,6 @@ public abstract class ActivityPackageDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             ActivityPackageDatabase.class, "happyActiveDB")
                             .createFromAsset("database/happyActiveDB.db")
-                            .addMigrations(MIGRATION_1_2)
                             .build();
                 }
             }
