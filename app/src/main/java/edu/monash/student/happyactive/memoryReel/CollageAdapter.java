ddbc7f6c8f44c9ccb02f98a73f59439846ff70f9
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-05-09 12:22:50 +1000

    write an adapter for handling session data

--- app/src/main/java/edu/monash/student/happyactive/memoryReel/CollageAdapter.java
+++ app/src/main/java/edu/monash/student/happyactive/memoryReel/CollageAdapter.java
@@ -1,4 +1,50 @@
 package edu.monash.student.happyactive.memoryReel;
 
-public class CollageAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.monash.student.happyactive.R;
+import edu.monash.student.happyactive.data.relationships.ActivitySessionWithPhotos;
+
+public class CollageAdapter extends RecyclerView.Adapter<CollageAdapter.CollageViewHolder>  {
+    List<ActivitySessionWithPhotos> sessionWithPhotosList = new ArrayList<>();
+
+    public static class CollageViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public View view;
+        public CollageViewHolder(View v) {
+            super(v);
+            view = v;
+        }
+    }
+
+    public CollageAdapter(List<ActivitySessionWithPhotos> myDataSet) {
+        sessionWithPhotosList = myDataSet;
+    }
+
+    @NonNull
+    @Override
+    public CollageAdapter.CollageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_memory_reel_collage, parent, false);
+        CollageAdapter.CollageViewHolder vh = new CollageAdapter.CollageViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CollageAdapter.CollageViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return sessionWithPhotosList.size();
+    }
 }
--- app/src/main/java/edu/monash/student/happyactive/memoryReel/PhotoCollageFragment.java
+++ app/src/main/java/edu/monash/student/happyactive/memoryReel/PhotoCollageFragment.java
@@ -3,17 +3,27 @@ package edu.monash.student.happyactive.memoryReel;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import edu.monash.student.happyactive.R;
+import edu.monash.student.happyactive.data.relationships.ActivitySessionWithPhotos;
 import edu.monash.student.happyactive.fragments.CameraFragmentArgs;
 
 
 public class PhotoCollageFragment extends Fragment {
 
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager layoutManager;
+    private CollageAdapter mCollageAdapter;
 
     public PhotoCollageFragment() {
         // Required empty public constructor
@@ -34,6 +44,21 @@ public class PhotoCollageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_photo_collage, container, false);
+        View view = inflater.inflate(R.layout.fragment_photo_collage, container, false);
+        recyclerView = (RecyclerView) view.findViewById(R.id.collageMemoryReelContainer);
+        recyclerView.setHasFixedSize(true);
+       // mSessionViewModel.getAllCompletedSessions(ActivityPackageStatus.COMPLETED).observe(getViewLifecycleOwner(), memoryReelObserver);
+        layoutManager = new LinearLayoutManager(getContext());
+        return view;
     }
+
+    Observer<List<ActivitySessionWithPhotos>> collageObserver = new Observer<List<ActivitySessionWithPhotos>>() {
+        @Override
+        public void onChanged(List<ActivitySessionWithPhotos> sessions) {
+            mCollageAdapter = new CollageAdapter(sessions);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(mCollageAdapter);
+        }
+    };
+
 }
