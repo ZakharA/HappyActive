ecd2eebdabc24baefe00a481d974cef4d5dcbcc8
Arsentii Zakharchenko <arsentii.zakharchenko@gmail.com>
2020-05-10 13:07:38 +1000

    write an adapter for handling session data'

--- app/src/main/java/edu/monash/student/happyactive/memoryReel/CollageAdapter.java
+++ app/src/main/java/edu/monash/student/happyactive/memoryReel/CollageAdapter.java
@@ -1,12 +1,26 @@
 package edu.monash.student.happyactive.memoryReel;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +28,7 @@ import edu.monash.student.happyactive.R;
 import edu.monash.student.happyactive.data.relationships.ActivitySessionWithPhotos;
 
 public class CollageAdapter extends RecyclerView.Adapter<CollageAdapter.CollageViewHolder>  {
-    List<ActivitySessionWithPhotos> sessionWithPhotosList = new ArrayList<>();
+    List<ActivitySessionWithPhotos> sessionWithPhotosList;
 
     public static class CollageViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
@@ -40,8 +54,22 @@ public class CollageAdapter extends RecyclerView.Adapter<CollageAdapter.CollageV
 
     @Override
     public void onBindViewHolder(@NonNull CollageAdapter.CollageViewHolder holder, int position) {
+        TextView titleView = (TextView) holder.view.findViewById(R.id.collage_activity);
+        TextView dateView = (TextView) holder.view.findViewById(R.id.collage_date);
+        ImageView imageView = (ImageView) holder.view.findViewById(R.id.collage_image);
+        String pattern = " dd MMMM yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(sessionWithPhotosList.get(position).activitySession.completedDateTime);
+        dateView.setText(date.toString());
+
+        if(sessionWithPhotosList.get(position).sessionPhoto.size() > 0){
+            String photoPath = holder.view.getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES) +"/" + sessionWithPhotosList.get(position).sessionPhoto.get(0).path;
+            Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
+            imageView.setImageBitmap(bitmap);
+        }
 
     }
+
 
     @Override
     public int getItemCount() {
